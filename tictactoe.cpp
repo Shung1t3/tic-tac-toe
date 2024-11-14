5326a0f3f3d058e45c34b07e4c235a6ae79ddb0d
Liam <debevecliam@gmail.com>
2024-11-14 12:50:53 -0500

    made a skeleton of the game

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -10,11 +10,12 @@ int board[25] = {
 
 };
 */
-
+enum {NOUGHTS , CROSSES , BORDER , EMPTY };
+enum { HUMANWIN, COMPWIN, DRAW };
 const int NOUGHTS = 1;
 const int CROSSES = 2;
 const int BORDER = 3;
-const int empty = 0;
+const int EMPTY = 0;
 
 const int ConvertTo25[9] = {
         6,7,8,
@@ -28,10 +29,24 @@ void InitializeBoard(int *board){
         board[i] = BORDER;
     }
     for (i = 0; i < 9; i++){
-        board[i] = BORDER;
+        board[ConvertTo25[i]] = EMPTY;
 }
+}
+void PrintBoard(const int *board){
+    int index = 0;
+    printf("\n Board: \n");
+    for (index = 0; index < 25; index++){
+        
+    if(index != 0 && index%5==0 ){
+        printf("\n");
+    }
+    printf("%4d", board[index]);
+    };
+    printf("\n");
 }
 int main(){
-
-
+int board[25];
+InitializeBoard(&board[0]);
+PrintBoard(&board[0]);
+return 0;
 }
